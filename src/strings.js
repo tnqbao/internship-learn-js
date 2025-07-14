2e4f0a6e95f02b68a15a316b03157e6c45f1a83c
Tran Nguyen Quoc Bao <quocbao.job106204@gmail.com>
2025-07-14 16:42:57 +0700

    feature: strings example

---
+++ src/strings.js
@@ -0,0 +1,12 @@
+const name = "Quoc Bao"
+const age = 20
+const address = "Da Nang, Vietnam"
+const email = "tnqb.bot@gmail.com"
+const phone = "0123456789"
+
+const introduction = "{ name: " + name + ", age: " + age + ", address: " + address + ", email: " + email + ", phone: " + phone + " }"
+
+const otherIntroduction = `{ name: ${name}, age: ${age}, address: ${address}, email: ${email}, phone: ${phone} }`
+
+console.log(introduction)
+console.log(otherIntroduction)
